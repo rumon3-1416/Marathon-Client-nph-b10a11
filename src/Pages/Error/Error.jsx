b553dc3488899960826ebb8498c304410471c7be
s-h-rumon <shahidhasanruman3@gmil.com>
2025-02-12 00:10:57 +0600

    error page designed

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,14 +1,26 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Error = () => {
+  const navigate = useNavigate();
+
   useEffect(() => {
     document.title = '404 Not Found';
   }, []);
 
   return (
-    <div className="bg-[#f4f4f4] min-h-screen flex flex-col justify-center items-center">
-      <h2 className="text-red-500 text-3xl font-bold">Error 404</h2>
-      <h2 className="text-red-500 text-3xl font-bold mt-4">Page Not Found</h2>
+    <div className="bg-[#cbcbcb] min-h-screen flex flex-col justify-center items-center">
+      <h2 className="text-[#242424] text-6xl font-semibold">404</h2>
+      <h2 className="text-[#4f4f4f] text-lg font-medium mt-2">
+        {`Oops! The Page you requested doesn't exists.`}
+      </h2>
+
+      <button
+        onClick={() => navigate('/')}
+        className="text-white bg-green font-semibold px-5 py-2 mt-4 rounded-lg"
+      >
+        Go Home
+      </button>
     </div>
   );
 };
