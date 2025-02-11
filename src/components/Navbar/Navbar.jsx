f26e4b481df20f8af24b18e53ff78be943190640
s-h-rumon <shahidhasanruman3@gmil.com>
2025-02-12 00:02:55 +0600

    navar updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,47 @@
 import React, { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import {
+  NavLink,
+  useLocation,
+  useNavigate,
+  useSearchParams,
+} from 'react-router-dom';
 
 import { useAuthContext } from '../../Hooks/useAuthContext';
 import MainLayout from '../../Layouts/MainLayout';
 import logo from '../../assets/icons/logo.png';
-// import lightIcon from '../../assets/icons/light.png';
-// import darkIcon from '../../assets/icons/dark.png';
 import userIcon from '../../assets/icons/user.png';
 import menuIcon from '../../assets/icons/menu.png';
+import { scroller } from 'react-scroll';
 
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
-
-  const { user, darkTheme, serverUrl, signOutUser } = useAuthContext();
+  const { user, darkTheme, signOutUser } = useAuthContext();
+  const { pathname } = useLocation();
   const navigate = useNavigate();
 
-  // const handleTheme = () => {
-  //   document.body.style.backgroundColor = darkTheme ? '#f7f7f7' : '#303030';
-  //   window.document.documentElement.classList.remove(
-  //     'bg-[#f7f7f7]',
-  //     'bg-dark3'
-  //   );
-  //   window.document.documentElement.classList.add(
-  //     darkTheme ? 'bg-[#f7f7f7]' : 'bg-dark3'
-  //   );
-  //   setDarkTheme(!darkTheme);
-  // };
+  const handleGoSection = secId => {
+    if (pathname === '/') {
+      scroller.scrollTo(secId, {
+        smooth: true,
+        duration: 500,
+      });
+    } else {
+      navigate('/', { state: { scrollTo: secId } });
+    }
+
+    setShowNav(false);
+  };
 
   return (
     <div className="w-full fixed top-0 inset-x-0 z-20">
       <div
         className={`w-full backdrop-blur-lg ${
-          darkTheme ? 'bg-dark5Trans' : 'bg-[#d9ebdea2]'
+          darkTheme ? 'bg-dark5Trans' : 'bg-[#cfead7ba]'
         }`}
       >
         <MainLayout>
           <nav
-            className={`h-24 py-6 flex justify-between items-center gap-2 relative`}
+            className={`h-20 py-5 flex justify-between items-center gap-2 relative`}
           >
             {/* Logo */}
             <div
@@ -47,65 +52,71 @@ const Navbar = () => {
               <img className="h-8 relative right-1.5" src={logo} alt="" />
             </div>
 
-            <div className="flex items-center gap-6">
-              <ul
-                className={`text-white bg-greenTrans sm:bg-transparent backdrop-blur-md sm:backdrop-blur-none sm:text-sm lg:text-base font-medium py-8 sm:py-0 rounded-xl overflow-hidden sm:flex flex-col sm:flex-row items-center gap-4 sm:gap-2 lg:gap-4 xl:gap-8 absolute sm:static inset-x-0 top-24 sm:top-0 z-30 ${
-                  showNav ? 'flex' : 'hidden'
-                }
-            ${darkTheme ? 'sm:text-lightTrans' : 'sm:text-[#32443f]'}`}
+            <ul
+              className={`text-[#32443f] bg-[#cfead7f7] md:bg-transparent backdrop-blur-md md:backdrop-blur-none md:text-sm lg:text-base font-medium md:h-fit md:py-0 rounded-b-xl overflow-hidden flex flex-col md:flex-row items-center gap-4 md:gap-2 lg:gap-4 xl:gap-8 absolute md:static inset-x-0 top-20 md:top-0 z-30 transition-all duration-300 ${
+                showNav ? 'h-72 py-8' : 'h-0'
+              }`}
+            >
+              <li onClick={() => setShowNav(false)} className="hover:text-gold">
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li onClick={() => setShowNav(false)} className="hover:text-gold">
+                <NavLink to="/all_marathons">Marathons</NavLink>
+              </li>
+              <li
+                onClick={() => handleGoSection('upcoming')}
+                className="hover:text-gold cursor-pointer"
               >
-                <li
-                  onClick={() => setShowNav(false)}
-                  className="hover:text-gold"
-                >
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li
-                  onClick={() => setShowNav(false)}
-                  className="hover:text-gold"
-                >
-                  <NavLink to="/all_marathons">Marathons</NavLink>
-                </li>
-                {user ? (
-                  <>
-                    <li
-                      onClick={() => setShowNav(false)}
-                      className="hover:text-gold"
-                    >
-                      <NavLink to="/dashboard">Dashboard</NavLink>
-                    </li>
-                    <li
-                      onClick={() => {
-                        signOutUser();
-                        setShowNav(false);
-                      }}
-                      className="hover:text-gold cursor-pointer sm:hidden"
-                    >
-                      Logout
-                    </li>
-                  </>
-                ) : (
-                  <>
-                    <li
-                      onClick={() => setShowNav(false)}
-                      className="hover:text-gold sm:hidden"
-                    >
-                      <NavLink to="/dashboard">Login</NavLink>
-                    </li>
-                    <li
-                      onClick={() => setShowNav(false)}
-                      className="hover:text-gold sm:hidden"
-                    >
-                      <NavLink to="/dashboard">Register</NavLink>
-                    </li>
-                  </>
-                )}
-              </ul>
+                Upcoming
+              </li>
 
+              <li
+                onClick={() => handleGoSection('help')}
+                className="hover:text-gold cursor-pointer"
+              >
+                Help
+              </li>
+              {user ? (
+                <>
+                  <li
+                    onClick={() => setShowNav(false)}
+                    className="hover:text-gold"
+                  >
+                    <NavLink to="/dashboard">Dashboard</NavLink>
+                  </li>
+                  <li
+                    onClick={() => {
+                      signOutUser();
+                      setShowNav(false);
+                    }}
+                    className="hover:text-gold cursor-pointer md:hidden"
+                  >
+                    Logout
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li
+                    onClick={() => setShowNav(false)}
+                    className="hover:text-gold md:hidden"
+                  >
+                    <NavLink to="/dashboard">Login</NavLink>
+                  </li>
+                  <li
+                    onClick={() => setShowNav(false)}
+                    className="hover:text-gold md:hidden"
+                  >
+                    <NavLink to="/dashboard">Register</NavLink>
+                  </li>
+                </>
+              )}
+            </ul>
+
+            <div className="flex items-center gap-6">
               <div className="flex items-center gap-3">
                 {user ? (
                   <>
-                    <button className="bg-transparent w-12 h-12 p-0.5 border-2 border-green hover:border-gold rounded-full">
+                    <button className="bg-transparent w-12 h-12 p-0.5 border-2 border-green rounded-full cursor-default">
                       <img
                         className="w-full h-full object-cover rounded-full"
                         referrerPolicy="no-referrer"
@@ -115,13 +126,13 @@ const Navbar = () => {
                     </button>
                     <button
                       onClick={() => signOutUser()}
-                      className="text-green hover:text-gold2 font-medium sm:px-4 md:px-6 py-2 border-2 border-green hover:border-gold2 rounded-lg hidden sm:block"
+                      className="text-green hover:text-gold2 font-medium sm:px-4 md:px-6 py-2 border-2 border-green hover:border-gold2 rounded-lg hidden md:block"
                     >
                       Logout
                     </button>
                   </>
                 ) : (
-                  <div className="hidden sm:flex items-center">
+                  <div className="hidden md:flex items-center">
                     {/* Login */}
                     <button
                       onClick={() => navigate('/signin')}
@@ -142,7 +153,7 @@ const Navbar = () => {
                 {/* Menubar */}
                 <button
                   onClick={() => setShowNav(!showNav)}
-                  className="p-2.5 border-2 border-green rounded-full sm:hidden"
+                  className="p-2.5 border-2 border-green rounded-full md:hidden"
                 >
                   <img className="w-6" src={menuIcon} alt="menu" />
                 </button>
