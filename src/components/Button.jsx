f4ab884f52359c909e526b977ba231293dcc14fb
s-h-rumon <shahidhasanruman3@gmil.com>
2025-04-24 10:52:47 +0600

    Updated: Navbar polished done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ onClick, className, children }) => {
   return (
     <button
       onClick={onClick}
-      className={`bg-green text-white hover:bg-gold2 text-sm font-medium px-6 py-2 rounded-sm transition-all duration-300 ${className}`}
+      className={`bg-green text-white hover:bg-dark-green text-sm font-medium px-6 py-2 rounded-sm transition-all duration-300 ${className}`}
     >
       {children}
     </button>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { scroller } from 'react-scroll';
 import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md';
+import { IoIosMenu } from 'react-icons/io';
 
 import { useAuthContext } from '../../Hooks/useAuthContext';
 import Container from '../../Layouts/Container';
@@ -68,86 +69,82 @@ const Navbar = () => {
       }`}
     >
       <div
-        className={`w-full backdrop-blur-lg ${
-          darkTheme ? 'bg-dark5Trans' : 'bg-[#f0fffeb7]'
+        className={`w-full ${
+          darkTheme ? 'bg-[#343434]' : 'bg-white'
         } shadow-lg`}
       >
         <Container>
           <nav
-            className={`h-20 py-5 flex justify-between items-center gap-2 relative`}
+            className={`h-16 py-5a flex justify-between items-center gap-2 relative`}
           >
             {/* Logo */}
             <div
               onClick={() => navigate('/')}
               className="cursor-pointer flex items-center gap-0"
             >
-              <h2 className="text-green text-xl font-bold">RunSphere</h2>
+              <h2
+                className={`text-xl font-bold ${
+                  darkTheme ? 'text-white' : 'text-gray-900'
+                }`}
+              >
+                RunSphere
+              </h2>
               <img className="h-8 relative right-1.5" src={logo} alt="" />
             </div>
 
+            {/* Nav Menu */}
             <ul
-              className={`md:bg-transparent backdrop-blur-md md:backdrop-blur-none md:text-sm lg:text-base font-medium md:h-fit md:py-0 rounded-b-xl shadow-lg md:shadow-none overflow-hidden flex flex-col md:flex-row items-center gap-4 md:gap-2 lg:gap-4 xl:gap-8 absolute md:static inset-x-0 top-20 md:top-0 z-30 transition-all duration-300 ${
-                showMenu ? 'h-72 py-8' : 'h-0'
+              className={`md:bg-transparent backdrop-blur-md md:backdrop-blur-none md:text-sm lg:text-base font-medium md:h-fit md:py-0 rounded-b-xl shadow-lg md:shadow-none overflow-hidden flex flex-col md:flex-row items-center gap-4 md:gap-2 lg:gap-4 xl:gap-8 absolute md:static inset-x-0 top-16 md:top-0 z-30 transition-all duration-300 ${
+                showMenu ? `${user ? 'h-60' : 'h-[17rem]'} py-6` : 'h-0'
               } ${
                 darkTheme
-                  ? 'text-light2 bg-[#464646f6]'
-                  : 'text-[#32443f] bg-[#cfead7f7]'
+                  ? 'text-light2 bg-[#343434]'
+                  : 'text-gray-800 bg-white'
               }`}
             >
               <li
                 onClick={() => setShowMenu(false)}
-                className="hover:text-gold"
+                className="hover:text-green transition-colors duration-300"
               >
                 <NavLink to="/">Home</NavLink>
               </li>
               <li
                 onClick={() => setShowMenu(false)}
-                className="hover:text-gold"
+                className="hover:text-green transition-colors duration-300"
               >
                 <NavLink to="/all_marathons">Marathons</NavLink>
               </li>
               <li
                 onClick={() => handleGoSection('upcoming')}
-                className="hover:text-gold cursor-pointer"
+                className="hover:text-green transition-colors duration-300 cursor-pointer"
               >
                 Upcoming
               </li>
 
               <li
                 onClick={() => handleGoSection('help')}
-                className="hover:text-gold cursor-pointer"
+                className="hover:text-green transition-colors duration-300 cursor-pointer"
               >
                 Help
               </li>
               {user ? (
-                <>
-                  <li
-                    onClick={() => setShowMenu(false)}
-                    className="hover:text-gold"
-                  >
-                    <NavLink to="/dashboard">Dashboard</NavLink>
-                  </li>
-                  <li
-                    onClick={() => {
-                      signOutUser();
-                      setShowMenu(false);
-                    }}
-                    className="hover:text-gold cursor-pointer md:hidden"
-                  >
-                    Logout
-                  </li>
-                </>
+                <li
+                  onClick={() => setShowMenu(false)}
+                  className="hover:text-green transition-colors duration-300"
+                >
+                  <NavLink to="/dashboard">Dashboard</NavLink>
+                </li>
               ) : (
                 <>
                   <li
                     onClick={() => setShowMenu(false)}
-                    className="hover:text-gold md:hidden"
+                    className="hover:text-green transition-colors duration-300 md:hidden"
                   >
                     <NavLink to="/signin">Login</NavLink>
                   </li>
                   <li
                     onClick={() => setShowMenu(false)}
-                    className="hover:text-gold md:hidden"
+                    className="hover:text-green transition-colors duration-300 md:hidden"
                   >
                     <NavLink to="/signup">Register</NavLink>
                   </li>
@@ -163,16 +160,18 @@ const Navbar = () => {
                     localStorage.setItem('darkTheme', darkTheme ? '' : true);
                     setDarkTheme(!darkTheme);
                   }}
-                  className={`text-3xl ${
-                    darkTheme ? 'text-white' : 'text-dark'
+                  className={`text-2xl p-1 rounded-full ${
+                    darkTheme
+                      ? 'text-white bg-white/10'
+                      : 'text-dark bg-black/10'
                   }`}
                 >
                   {darkTheme ? <MdOutlineLightMode /> : <MdOutlineDarkMode />}
                 </button>
 
                 {user ? (
-                  <>
-                    <button className="bg-transparent w-12 h-12 p-0.5 border-2 border-green rounded-full cursor-default">
+                  <div className="flex flex-col justify-center items-center relative group">
+                    <button className="bg-transparent w-9 h-9 p-0.5 border-[1.5px] border-green rounded-full">
                       <img
                         className="w-full h-full object-cover rounded-full"
                         referrerPolicy="no-referrer"
@@ -180,22 +179,29 @@ const Navbar = () => {
                         alt=""
                       />
                     </button>
-                    <button
-                      onClick={() => signOutUser()}
-                      className="text-green hover:text-gold2 font-medium sm:px-4 lg:px-6 py-2 border-2 border-green hover:border-gold2 rounded-lg hidden md:block"
-                    >
-                      Logout
-                    </button>
-                  </>
+
+                    <div className="pt-10 absolute top-0 right-0 hidden group-hover:block">
+                      <div
+                        className={`bg-white w-24 h-12 border border-gray-300 rounded-md flex flex-col justify-center items-center`}
+                      >
+                        <button
+                          className="text-sm font-medium hover:text-red-800"
+                          onClick={signOutUser}
+                        >
+                          Logout
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 ) : (
                   <div className="hidden md:flex items-center">
                     {/* Login */}
                     <button
                       onClick={() => navigate('/signin')}
-                      className={`text-sm sm:text-base font-medium ps-3 sm:ps-6 lg:ps-8 pe-2 sm:pe-4 lg:pe-6 py-2 border-2 rounded-s-lg ${
+                      className={`text-sm font-medium ps-3 sm:ps-6 lg:ps-8 pe-2 sm:pe-4 lg:pe-6 py-1.5 border-2 rounded-s-sm ${
                         pathname === '/signin'
-                          ? 'text-gold2 border-gold2'
-                          : 'text-green hover:text-gold2 border-green hover:border-gold2'
+                          ? 'text-dark-green border-dark-green'
+                          : 'text-green hover:text-dark-green border-green hover:border-dark-green'
                       }`}
                     >
                       Login
@@ -203,10 +209,10 @@ const Navbar = () => {
                     {/* Register */}
                     <button
                       onClick={() => navigate('/signup')}
-                      className={`text-white text-sm sm:text-base font-medium ps-2 sm:ps-4 lg:ps-6 pe-3 sm:pe-6 lg:pe-8 py-2  border-2 rounded-e-lg ${
+                      className={`text-white text-sm font-medium ps-2 sm:ps-4 lg:ps-6 pe-3 sm:pe-6 lg:pe-8 py-1.5 border-2 rounded-e-sm ${
                         pathname === '/signup'
-                          ? 'bg-gold2 border-gold2'
-                          : 'bg-green hover:bg-gold2 border-green hover:border-gold2'
+                          ? 'bg-dark-green border-dark-green'
+                          : 'bg-green hover:bg-dark-green border-green hover:border-dark-green'
                       }`}
                     >
                       Register
@@ -217,9 +223,13 @@ const Navbar = () => {
                 {/* Menubar */}
                 <button
                   onClick={() => setShowMenu(!showMenu)}
-                  className="p-2.5 border-2 border-green rounded-full md:hidden"
+                  className={`text-2xl p-1 border-[1.5px] rounded-full md:hidden ${
+                    darkTheme
+                      ? 'text-light2 border-light2'
+                      : 'text-gray-900 border-gray-800'
+                  }`}
                 >
-                  <img className="w-6" src={menuIcon} alt="menu" />
+                  <IoIosMenu />
                 </button>
               </div>
             </div>
@@ -227,7 +237,7 @@ const Navbar = () => {
         </Container>
       </div>
 
-      <div className="h-20"></div>
+      <div className="h-16"></div>
     </div>
   );
 };
